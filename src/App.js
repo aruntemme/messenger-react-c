f94f2cbe012c52e4034cf2666166c447c5e61aaa
aruntemme <arungauthamk@gmail.com>
2020-08-30 20:32:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
+//              FUNCTION STARTS HERE
 function App() {
+  //              REACT HOOKS HERE
+  const [input, setInput] = useState("");
+
+  //              RETURN
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello fellows</h1>
+
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
